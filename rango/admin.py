151a8f55fbf918ac1eebd1e456f5ac49429950d8
GerganaIvanova <2395634I@student.gla.ac.uk>
2020-02-11 16:48:18 +0000

    Chapter 9 completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin 
 from rango.models import Category, Page
+from rango.models import UserProfile
  
 
 class PageAdmin(admin.ModelAdmin):
@@ -9,5 +10,7 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Page, PageAdmin)
-admin.site.register(Category, CategoryAdmin) 
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
+
 
--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User 
+from rango.models import UserProfile
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,
@@ -43,4 +46,17 @@ class PageForm(forms.ModelForm):
         # we can either exclude the category field from the form,
         exclude = ('category',)
         # or specify the fields to include (don't include the category field).
-        # fields = ('title', 'url', 'views')   
+        # fields = ('title', 'url', 'views')
+
+class UserForm(forms.ModelForm): 
+    password = forms.CharField(widget=forms.PasswordInput())
+    
+    class Meta: 
+        model = User 
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm): 
+    class Meta: 
+        model = UserProfile 
+        fields = ('website', 'picture',)
+   
